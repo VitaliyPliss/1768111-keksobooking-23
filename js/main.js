819d455f5d1a73313e6d0ba7c24a096bf1c2c506
VitaliyPliss <v.pliss@mail.ru>
2021-06-25 14:34:05 +0300

    создан модуль генерации разметки. задание 7.9 исправлены недочеты

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 import {similarFlat} from './utils/get-flat.js';
 import './utils/get-similar-flat.js';
-
-console.log(similarFlat);
--- js/utils/get-similar-flat.js
+++ js/utils/get-similar-flat.js
@@ -2,52 +2,78 @@
 /* eslint-disable id-length */
 import {similarFlat} from './get-flat.js';
 
-const POPUP = document.querySelector('#map-canvas');
-const TEMPLATE = document.querySelector('#card').content;
+const popup = document.querySelector('#map-canvas');
+const template = document.querySelector('#card').content;
 const NEW_OFFER = similarFlat();
-
-NEW_OFFER.forEach((flat) => {
+const FIRST_OFFER = NEW_OFFER[0];
+const createNewOffer = function (flat){
   //ищем элементы по классу
-  const COPY = TEMPLATE.cloneNode(true);
-  const TITLE = COPY.querySelector('.popup__title');
-  const ADDRESS = COPY.querySelector('.popup__text--address');
-  const PRICE = COPY.querySelector('.popup__text--price');
-  const TYPE = COPY.querySelector('.popup__type');
-  const CAPACITY = COPY.querySelector('.popup__text--capacity');
-  const TIME = COPY.querySelector('.popup__text--time');
-  const FEATURES = COPY.querySelector('.popup__features');
-  const DESCRIPTION = COPY.querySelector('.popup__description');
-  const PHOTOS = COPY.querySelector('.popup__photos');
-  const PHOTO = COPY.querySelectorAll('.popup__photo');
-  const AVATAR = COPY.querySelector('.popup__avatar');
+  const copy = template.cloneNode(true);
+  const title = copy.querySelector('.popup__title');
+  const address = copy.querySelector('.popup__text--address');
+  const price = copy.querySelector('.popup__text--price');
+  const type = copy.querySelector('.popup__type');
+  const capacity = copy.querySelector('.popup__text--capacity');
+  const time = copy.querySelector('.popup__text--time');
+  const features = copy.querySelector('.popup__features');
+  const feature = copy.querySelectorAll('.popup__feature');
+  const description = copy.querySelector('.popup__description');
+  const photos = copy.querySelector('.popup__photos');
+  const photo = copy.querySelectorAll('.popup__photo');
+  const avatar = copy.querySelector('.popup__avatar');
 
   //присваеваем значения элементам
-  TITLE.textContent = flat.offer.title;
-  ADDRESS.textContent = flat.offer.address;
-  PRICE.textContent = `${flat.offer.price  } ₽/ночь`;
+  title.textContent = flat.offer.title;
+  address.textContent = flat.offer.address;
+  price.textContent = `${flat.offer.price  } ₽/ночь`;
   switch (flat.offer.type) {
     case 'flat':
-      TYPE.textContent = 'Квартира';
+      type.textContent = 'Квартира';
       break;
     case 'bungalow':
-      TYPE.textContent = 'Бунгало';
+      type.textContent = 'Бунгало';
       break;
     case 'house':
-      TYPE.textContent = 'Дом';
+      type.textContent = 'Дом';
       break;
     case 'palace':
-      TYPE.textContent = 'Дворец';
+      type.textContent = 'Дворец';
       break;
     case 'hotel':
-      TYPE.textContent = 'Отель';
+      type.textContent = 'Отель';
       break;
   }
-  CAPACITY.textContent = `${flat.offer.rooms  } комнаты для ${  flat.offer.guests  } гостей`;
-  TIME.textContent = `Заезд после ${  flat.offer.checkin  } выезд до ${  flat.offer.checkout}`;
-  FEATURES.textContent = flat.offer.features;
-  DESCRIPTION.textContent = flat.offer.description;
-  AVATAR.src = flat.author.avatar;
-  PHOTO[0].src = flat.offer.photos[0];
+  capacity.textContent = `${flat.offer.rooms  } комнаты для ${  flat.offer.guests  } гостей`;
+  time.textContent = `Заезд после ${  flat.offer.checkin  } выезд до ${  flat.offer.checkout}`;
+  for (let i = 0; i<flat.offer.features.length; i++) {
+    if (flat.offer.features[i] !== 'wifi') {
+      feature[i].classList.remove('popup__feature');
+      feature[i].classList.remove('popup__feature--wifi');
+    }
+    if (flat.offer.features[i] !== 'parking') {
+      feature[i].classList.remove('popup__feature');
+      feature[i].classList.remove('popup__feature--parking');
+    }
+    if (flat.offer.features[i] !== 'dishwasher' ) {
+      feature[i].classList.remove('popup__feature');
+      feature[i].classList.remove('popup__feature--dishwasher');
+    }
+    if (flat.offer.features[i] !== 'washer' ) {
+      feature[i].classList.remove('popup__feature');
+      feature[i].classList.remove('popup__feature--washer');
+    }
+    if (flat.offer.features[i] !== 'elevator') {
+      feature[i].classList.remove('popup__feature');
+      feature[i].classList.remove('popup__feature--elevator');
+    }
+    if (flat.offer.features[i] !== 'conditioner') {
+      feature[i].classList.remove('popup__feature');
+      feature[i].classList.remove('popup__feature--conditioner');
+    }
+  }
+  description.textContent = flat.offer.description;
+  avatar.src = flat.author.avatar;
+  photo[0].src = flat.offer.photos[0];
   for(let i = 1; i<flat.offer.photos.length; i++) {
     if(flat.offer.photos[i] !== undefined) {
       const PICTURE = document.createElement('img');
@@ -56,16 +82,17 @@ NEW_OFFER.forEach((flat) => {
       PICTURE.width = '45';
       PICTURE.height = '40';
       PICTURE.src = flat.offer.photos[i];
-      PHOTOS.appendChild(PICTURE);
+      photos.appendChild(PICTURE);
     }
   }
+
   // Скрываем блоки при отсутствии информации
-  const ELEMENTS = [TITLE, ADDRESS, PRICE, TYPE, CAPACITY, TIME, FEATURES, DESCRIPTION, AVATAR, PHOTOS];
+  const ELEMENTS = [title, address, price, type, capacity, time, features, description, avatar, photos];
   for(let i = 0; i<ELEMENTS.length; i++) {
     if(ELEMENTS[i].textContent === 0 || null || '' || undefined) {
       ELEMENTS[i].classList.add('hidden');
     }
   }
-  POPUP.appendChild(COPY);
-});
-
+  popup.appendChild(copy);
+};
+createNewOffer(FIRST_OFFER);
